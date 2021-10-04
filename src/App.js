216c360e57e620c71b744c4aeb78b9388bc26ab6
kwalorz <kmwalorz@gmail.com>
2021-10-03 22:45:28 -0400

    second materialUI practice project, first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant='h6'>Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth='sm'>
+            <Typography
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph
+            >
+              I am still learning this library so if you have a problem with how
+              it looks that is not my problem
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </Fragment>
   );
-}
+};
 
 export default App;
